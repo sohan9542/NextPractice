cecef5380a9da25450976810b0780f9584c7f895
sohan9542 <sohanurrahmants@gmail.com>
2022-01-14 18:57:34 +0600

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Head from "next/head"
+import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
+import useStyles from '../utills/styles'
+const Layout = ({ children }) => {
+    const classes = useStyles();
+    return (
+        <div>
+            <Head>
+                <title>Next Amazona</title>
+            </Head>
+            <AppBar position='static' className={classes.navbar}>
+                <Toolbar>
+                    <Typography>Amazona!</Typography>
+                </Toolbar>
+
+            </AppBar>
+            <Container className={classes.main}>
+                {children}
+            </Container>
+            <footer className={classes.footer}>
+                <Typography>All rights reserved. Next Amazona</Typography>
+            </footer>
+        </div>
+    )
+}
+
+export default Layout
