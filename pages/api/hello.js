4b8c3f264c6da3f091f6e371effb6ed7861875e1
sohan9542 <sohanurrahmants@gmail.com>
2022-01-19 20:40:31 +0600

    connecting to mongodb

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import db from "../../utills/db"
+
 export default function handler(req, res) {
+   db.connect()
+   db.disconnect()
   res.status(200).json({ name: 'John Doe' })
 }
 
